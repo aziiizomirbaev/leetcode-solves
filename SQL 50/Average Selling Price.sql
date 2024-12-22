636c629d58d0533777656437fd4f9f6efa0bce99
Aziz 'Isaac' Omirbaev <azizomirbaevahalo@gmail.com>
2024-12-22 22:10:56 +0500

    SQL 50 - 16th solution

---
+++ SQL 50/Average Selling Price.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.product_id, 
+    COALESCE(ROUND(
+        COALESCE(SUM(p.price * u.units), 0) * 1.00 / NULLIF(SUM(u.units), 0), 
+    2), 0) AS average_price
+FROM Prices AS p 
+LEFT JOIN UnitsSold AS u ON p.product_id = u.product_id AND 
+    u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id 
