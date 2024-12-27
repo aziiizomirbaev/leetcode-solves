43f59f961f9f06421603806db24fdd2ff756e917
Aziz 'Isaac' Omirbaev <azizomirbaevahalo@gmail.com>
2024-12-27 20:55:45 +0500

    SQL 50 - 20th solution

--- SQL 50/Monthly Transactions I.sql
+++ SQL 50/Monthly Transactions I.sql
@@ -5,5 +5,6 @@ SELECT
     COUNT(state) FILTER(WHERE state = 'approved') AS approved_count, 
     SUM(amount) AS trans_total_amount, 
     COALESCE(SUM(amount) FILTER (WHERE state = 'approved'), 0) AS approved_total_amount
+    
 FROM Transactions 
 GROUP BY month, country 
